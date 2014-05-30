2bc72b3fcc1e6f07648a6bbe082a2be59909fb51
sherl0ck044 <oleksiy97@gmail.com>
2014-05-30 10:28:37 +0300

    Added Ukrainian language

---
+++ language/uk/profile_side_switcher.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* profile_side_switcher [Ukrainian]
+*
+* @package language profile_side_switcher
+* @copyright (c) 2014 Oleksiy Fryschyn
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'PSS_LEFT'			=> 'Мініпрофілі ліворуч',
+	'PSS_RIGHT'			=> 'Мініпрофілі праворуч',
+));
