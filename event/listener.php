43f3221030d4a3a6e07b575346e8e5f330089a1f
Tatiana5 <shehovtzova.tanya@yandex.ru>
2014-07-19 17:39:13 +0400

    Update to phpBB 3.1.0 RC2

--- event/listener.php
+++ event/listener.php
@@ -16,18 +16,33 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {	
+	/** @var \phpbb\template\template */
+	protected $template;
+	
 	/** @var \phpbb\user */
 	protected $user;
+	
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+	
+	/** @var string */
+	protected $phpbb_root_path;
+	protected $php_ext;
 	
 	/**
 	* Constructor
 	* 
+	* @param \phpbb\template\template $template
 	* @param \phpbb\user $user
 	*/
 	
-	public function __construct(\phpbb\user $user)
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, $phpbb_root_path, $php_ext)
 	{
+		$this->template = $template;
 		$this->user = $user;
+		$this->db = $db;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
 	}
 	
 	/**
@@ -40,11 +55,66 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header'	=>	'profile_side_switcher',
+			'core.user_setup'					=> 'load_language_on_setup',
+			'core.viewtopic_modify_page_title'	=> 'profile_side_switcher',
+			'core.ucp_prefs_view_data'			=> 'ucp_profile_side_switcher_get',
+			'core.ucp_prefs_view_update_data'	=> 'ucp_profile_side_switcher_set',
+			'core.ucp_prefs_modify_common'		=> 'add_ucp_prefs_template_vars',
 		);
 	}
 	
+	public function load_language_on_setup($event) {
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'tatiana5/profile_side_switcher',
+			'lang_set' => 'profile_side_switcher',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+	
 	public function profile_side_switcher($event) {
-		$this->user->add_lang_ext('tatiana5/profile_side_switcher', 'profile_side_switcher');
+		global $request;
+		
+		$topic_data = $event['topic_data'];
+		$forum_id = $event['forum_id'];
+		
+		if ($request->is_ajax() && $request->is_set('pss'))
+		{
+			$pss_left = request_var('pss', 0);
+			$sql = 'UPDATE ' . USERS_TABLE . ' SET allow_pss_left = ' . (int) $pss_left;
+			$result = $this->db->sql_query($sql);
+			
+			$json_response = new \phpbb\json_response;
+			$json_response->send(array(
+				'success'		=> ($result) ? true : false,
+			));
+			
+			$this->db->sql_freeresult($result);
+		}
+		
+		$this->template->assign_vars(array(
+			'S_PSS_LEFT'	=> $this->user->data['allow_pss_left'],
+			'PSS_URL'		=> append_sid("{$this->phpbb_root_path}viewtopic.$this->php_ext", 'f=' . $forum_id . '&amp;t='. $topic_data['topic_id'] . '&amp;pss=1'),
+		));
+	}
+	
+	public function ucp_profile_side_switcher_get($event) {
+		$data = $event['data'];
+		$data = array_merge($data, array(
+			'allow_pss_left'		=> request_var('pss_left', (bool) (isset($this->user->data['allow_pss_left']) ? $this->user->data['allow_pss_left'] : false))
+		));
+		$event['data'] = $data;
+	}
+	
+	public function ucp_profile_side_switcher_set($event) {
+		$data = $event['data'];
+		$sql_ary = $event['sql_ary'];
+		if (isset($this->user->data['allow_pss_left']))
+		{
+			$sql_ary = array_merge($sql_ary, array(
+				'allow_pss_left'	=> ($data['allow_pss_left']) ? 1 : 0,
+			));
+		}
+		$event['sql_ary'] = $sql_ary;
 	}
 }
---
+++ language/ru/info_ucp_profile_side_swither.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* profile_side_switcher [Russian]
+*
+* @package language profile_side_switcher
+* @copyright (c) 2014 Татьяна5
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'PSS_VIEW_LEFT'			=> 'Показывать минипрофили слева',
+));
