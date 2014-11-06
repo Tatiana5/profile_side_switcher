ecba18bd86acebae390b2ba9b3e057840753c8a8
Tatiana5 <shehovtzova.tanya@yandex.ru>
2014-11-06 10:20:44 +0300

    Fix for travic-ci

--- event/listener.php
+++ event/listener.php
@@ -15,36 +15,40 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 * Event listener
 */
 class listener implements EventSubscriberInterface
-{	
+{
 	/** @var \phpbb\template\template */
 	protected $template;
-	
+
 	/** @var \phpbb\user */
 	protected $user;
-	
+
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
-	
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
 	/** @var string */
 	protected $phpbb_root_path;
 	protected $php_ext;
-	
+
 	/**
 	* Constructor
 	* 
 	* @param \phpbb\template\template $template
 	* @param \phpbb\user $user
 	*/
-	
-	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, $phpbb_root_path, $php_ext)
+
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, $phpbb_root_path, $php_ext)
 	{
 		$this->template = $template;
 		$this->user = $user;
 		$this->db = $db;
+		$this->request = $request;
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->php_ext = $php_ext;
 	}
-	
+
 	/**
 	* Assign functions defined in this class to event listeners in the core
 	*
@@ -61,55 +65,57 @@ class listener implements EventSubscriberInterface
 			'core.ucp_prefs_view_update_data'	=> 'ucp_profile_side_switcher_set',
 		);
 	}
-	
-	public function load_language_on_setup($event) {
+
+	public function load_language_on_setup($event)
+	{
 		$lang_set_ext = $event['lang_set_ext'];
 		$lang_set_ext[] = array(
 			'ext_name' => 'tatiana5/profileSideSwitcher',
 			'lang_set' => 'profile_side_switcher',
 		);
 		$event['lang_set_ext'] = $lang_set_ext;
-		
+	
 		$this->template->assign_vars(array(
 			'S_PSS_LEFT'	=> $this->user->data['allow_pss_left'],
 		));
 	}
-	
-	public function profile_side_switcher($event) {
-		global $request;
-		
+
+	public function profile_side_switcher($event)
+	{
 		$topic_data = $event['topic_data'];
 		$forum_id = $event['forum_id'];
-		
-		if ($request->is_ajax() && $request->is_set('pss'))
+
+		if ($this->request->is_ajax() && $this->request->is_set('pss'))
 		{
-			$pss_left = request_var('pss', 0);
+			$pss_left = $this->request->variable('pss', 0);
 			$sql = 'UPDATE ' . USERS_TABLE . ' SET allow_pss_left = ' . (int) $pss_left . ' WHERE user_id = ' . (int) $this->user->data['user_id'];
 			$result = $this->db->sql_query($sql);
-			
+
 			$json_response = new \phpbb\json_response;
 			$json_response->send(array(
 				'success'		=> ($result) ? true : false,
 			));
-			
+
 			$this->db->sql_freeresult($result);
 		}
-		
+
 		$this->template->assign_vars(array(
 			'PSS_URL_LEFT'		=> append_sid("{$this->phpbb_root_path}viewtopic.$this->php_ext", 'f=' . $forum_id . '&amp;t='. $topic_data['topic_id'] . '&amp;pss=1'),
 			'PSS_URL_RIGHT'		=> append_sid("{$this->phpbb_root_path}viewtopic.$this->php_ext", 'f=' . $forum_id . '&amp;t='. $topic_data['topic_id'] . '&amp;pss=0'),
 		));
 	}
-	
-	public function ucp_profile_side_switcher_get($event) {
+
+	public function ucp_profile_side_switcher_get($event)
+	{
 		$data = $event['data'];
 		$data = array_merge($data, array(
-			'allow_pss_left'		=> request_var('pss_left', (bool) (isset($this->user->data['allow_pss_left']) ? $this->user->data['allow_pss_left'] : false))
+			'allow_pss_left'		=> $this->request->variable('pss_left', (bool) (isset($this->user->data['allow_pss_left']) ? $this->user->data['allow_pss_left'] : false))
 		));
 		$event['data'] = $data;
 	}
-	
-	public function ucp_profile_side_switcher_set($event) {
+
+	public function ucp_profile_side_switcher_set($event)
+	{
 		$data = $event['data'];
 		$sql_ary = $event['sql_ary'];
 		if (isset($this->user->data['allow_pss_left']))
