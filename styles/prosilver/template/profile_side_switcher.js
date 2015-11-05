d6cc358e42b38de4913bb23947360ec9222c48da
lavigor <lavigor@users.noreply.github.com>
2015-11-06 01:59:13 +0300

    Check postprofile visibility.

---
+++ styles/prosilver/template/profile_side_switcher.js
@@ -0,0 +1,5 @@
+; (function ($, window, document) {
+	// do stuff here and use $, window and document safely
+	// https://www.phpbb.com/community/viewtopic.php?p=13589106#p13589106
+	$(".post:has(.postprofile:hidden)").addClass("profile_hidden");
+})(jQuery, window, document);
